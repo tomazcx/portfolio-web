933710d7c212fa575e621899efd85c641f591bcd
tomazcx <tomazcx06@gmail.com>
2023-02-11 12:33:45 -0300

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.tsx', './index.html'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: 'Inter, sans-serif'
+			},
+			colors: {
+				blue: {
+					400: '#315EFF',
+					500: '#1246FF',
+					600: '#2E4AAB'
+				},
+				background: '#1A1A1A',
+				white: '#F0F0F0'
+			}
+		},
+	},
+	plugins: [],
+}
